f76fcab585b84a92c0088e152fe64d9739a044ac
Tim Schreiber <timschreiber@users.noreply.github.com>
2017-10-19 11:57:55 -0400

    Added content to About section, ajustments to sidebar and connect button

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,21 @@
 (function($){
     $(function(){
         $('#menu-button').sideNav();
+        pulseConnectButton();
     });
+    
+    function pulseConnectButton() {
+        var pulses = 0;
+        var ntrvl = setInterval(function(){
+            if(pulses < 3) {
+            $('#btn-connect').addClass('pulse');
+                setTimeout(function(){
+                    $('#btn-connect').removeClass('pulse');
+                }, 2000);
+                pulses++;
+            } else {
+                clearInterval(ntrvl);
+            }
+        }, 7000);
+    }
 })(jQuery);
